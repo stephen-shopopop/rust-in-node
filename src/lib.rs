70d1fc5c3738cce543ff0edd2f97eef1c6f42777
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-09-18 12:49:59 +0200

    feat(bundle): ADD vscode config

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,18 @@ use napi_derive::napi;
 pub fn plus_100(input: u32) -> u32 {
   input + 100
 }
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn not_works() {
+    let result = 2 + 3;
+    assert_eq!(result, 4);
+  }
+
+  #[test]
+  fn it_works() {
+    let result = 2 + 2;
+    assert_eq!(result, 4);
+  }
+}
